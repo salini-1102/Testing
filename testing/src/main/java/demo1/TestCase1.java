ed9a6044187ac8c2b96ac36acb1cdeec00224248
salini <nagasalini87@gmail.com>
2023-01-09 20:08:47 +0530

    Pushing testing maven project

---
+++ testing/src/main/java/demo1/TestCase1.java
@@ -0,0 +1,15 @@
+package demo1;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class TestCase1 {
+@Test
+public void m1()
+{
+	Reporter.log("First TC running",true);
+}
+}
